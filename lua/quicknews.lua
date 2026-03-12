82d08fa9290a25a3e937f35e263243577db7f4f0
J Wayston <177466602+jwayston@users.noreply.github.com>
2026-03-12 17:12:09 +0200

    Parsing improvements, refactoring
    - Parsing is now more reliable and not depend on certain element order.
    - Parsing removes cdata slop when found.
    - Some general refactoring

--- lua/quicknews.lua
+++ lua/quicknews.lua
@@ -7,14 +7,7 @@
 --- @class QuickNews
 local M = {}
 
-local notify = vim.notify or print
 local namespace = vim.api.nvim_create_namespace("QuickNews")
-local win_opts = {
-    relative = "editor",
-    col = 0,
-    width = vim.o.columns,
-    style = "minimal",
-}
 
 --- @class Config
 M.config = {
@@ -35,20 +28,29 @@ M.setup = function(opts)
         function() M.get_news() end, { desc = "Show a RSS news popup" })
 
     vim.api.nvim_set_hl(namespace, "Underlined", { underline = M.config.style.underline })
-    win_opts.height = M.config.height
-    win_opts.row = vim.o.lines - win_opts.height
 end
 
 --- Print error message
 --- @param msg string Message
 local show_err = function(msg)
-    vim.notify("[QuickNews] Error: " .. msg, vim.log.levels.ERROR)
+    local notify = vim.notify or print
+    notify("[QuickNews] Error: " .. msg, vim.log.levels.ERROR)
 end
 
 --- Open a floating scratch popup window above the command line
+--- @param opts table Window options
 --- @return number buf The buffer id
 --- @return number win The window id
-local open_scratch_win = function()
+local open_scratch_win = function(opts)
+    local win_opts = vim.tbl_deep_extend("force", {
+        relative = "editor",
+        col = 0,
+        row = vim.o.lines - M.config.height,
+        width = vim.o.columns,
+        height = M.config.height,
+        style = "minimal",
+    }, opts or {})
+
     local buf = vim.api.nvim_create_buf(false, true)
     local win = vim.api.nvim_open_win(buf, true, win_opts)
 
@@ -67,35 +69,34 @@ end
 
 --- Parse the RSS feed
 --- @param data string Rss raw feed data
---- @return string[] # List of parsed & formatted news strings 
+--- @return table # <title, list of parsed & formatted news strings>
 local parse_rss_data = function(data)
-    local res = {}
-    local title_max_width = vim.o.columns - 20
-    local r_item = [[\v\<item\>\<title\>([^<]+)\<\/title\>]] ..
-                    [[\<link\>([^<]+)\<\/link\>]] ..
-                    [[.{-}\<pubDate\>([^<]+)\<\/pubDate\>]]
+    data = data:gsub("<!%[CDATA%[(.-)%]%]>", "%1")  -- Remove CDATA slop
+    local result = { title = nil, items = {} }
+    local headline_max_width = vim.o.columns - 20
 
-    win_opts.title = M.config.title or
-        data:match("<channel><title>(.-)</title>") or ""
+    local channel_block = data:match("<channel>(.-)</channel>")
+    result.title = channel_block:match("<title>(.-)</title>") or nil
+    local matches = data:gmatch("<item>(.-)</item>")
 
-    local matches = vim.fn.matchstrlist({data}, r_item, {submatches = true})
-    local max_items = M.config.max_items
-    local limit = (max_items > 0) and math.min(max_items, #matches) or #matches
-
-    for i = 1, limit do
-        local m = matches[i]
-        local unixtime = vim.fn.strptime("%a, %d %b %Y %T", m.submatches[3])
+    local i = 1
+    for item_block in matches do
+        local title = item_block:match("<title>(.-)</title>")
+        local link = item_block:match("<link>(.-)</title>")
+        local datetime = item_block:match("<pubDate>(.-)</pubDate>")
+        local unixtime = vim.fn.strptime("%a, %d %b %Y %T", datetime)
         local timestamp = os.date("%y-%m-%d %H:%M", unixtime)
-        local title = m.submatches[1]
-        local link = m.submatches[2]
 
-        title = #title > title_max_width and
-            (vim.fn.strcharpart(title, 0, title_max_width) .. "...") or title
-        table.insert(res, string.format("%s [%s](%s)", timestamp, title, link))
+        title = #title > headline_max_width and
+            (vim.fn.strcharpart(title, 0, headline_max_width) .. "...") or title
+        table.insert(result.items, string.format("%s [%s](%s)", timestamp, title, link))
+
+        if i >= M.config.max_items then break end
+        i = i + 1
     end
 
-    table.sort(res, function(a, b) return a > b end)
-    return res
+    table.sort(result.items, function(a, b) return a > b end)
+    return result
 end
 
 --- Get and show news from the RSS stream in a floating window
@@ -111,10 +112,10 @@ M.get_news = function()
     if not o.stdout then show_err("Empty RSS feed") return end
 
     local news = parse_rss_data(o.stdout)
-    if not news then show_err("RSS parsing failed") return end
+    if not news.items then show_err("RSS parsing failed") return end
 
-    local b, w = open_scratch_win()
-    vim.api.nvim_buf_set_lines(b, 0, -1, false, news)
+    local b, w = open_scratch_win({ title = M.config.title or news.title })
+    vim.api.nvim_buf_set_lines(b, 0, -1, false, news.items)
     vim.api.nvim_win_set_cursor(w, {1, 15})
 end
 
